12f4e6ccb9e6ff9a690f2f6e019d8598d45b9dd0
Marcus Näslund <marcus.naslund@kognity.com>
2023-12-28 20:04:44 +0100

    better ai and walls

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ buttonRestart.onclick = () => {
 const airock = document.getElementById('airock');
 const aiscissors = document.getElementById('aiscissors');
 const aipaper = document.getElementById('aipaper');
+const countrock = document.getElementById('countrock');
+const countscissors = document.getElementById('countscissors');
+const countpaper = document.getElementById('countpaper');
 
 const iso = new Isomer(document.getElementById("art"));
 
@@ -21,21 +24,47 @@ const BLUE = new Isomer.Color(100, 100, 200, 0.6);
 
 let simulationRunning = true;
 
-const angleTo = (currentX, currentY, targetX, targetY) => {
-    return (2 * Math.PI + Math.atan2((targetY - currentY), (targetX - currentX))) % (2 * Math.PI);
+const zDistance = ({x, y, size}) => {
+    return Math.sqrt((x + size/2)**2 + (y + size/2)**2);
 }
 
-const distanceTo = (currentX, currentY, targetX, targetY) => {
+const isEnemy = (obj, other) => {
+    return (obj.team === 'rock' && other.team === 'scissors') ||
+           (obj.team === 'scissors' && other.team === 'paper') ||
+           (obj.team === 'paper' && other.team === 'rock');
+}
+
+const angleTo = (current, target) => {
+    const {x: currentX, y: currentY} = current;
+    const {x: targetX, y: targetY} = target;
+    const result = (2 * Math.PI + Math.atan2((targetY - currentY), (targetX - currentX))) % (2 * Math.PI);
+    return result;
+}
+
+const distanceTo = (current, target) => {
+    const {x: currentX, y: currentY} = current;
+    const {x: targetX, y: targetY} = target;
     return Math.sqrt(((targetY - currentY)**2) + ((targetX - currentX)**2));
 }
 
-const nearestOfType = (currentX, currentY, team) => {
+const nearestOfType = (obj, team) => {
     const filtered = objects.filter(obj => obj.team === team);
     if (filtered.length === 0) {
         return null;
     }
-    filtered.sort((a, b) => {distanceTo(currentX, currentY, a.x, a.y) - distanceTo(currentX, currentY, b.x, b.y)});
+    filtered.sort((a, b) => {distanceTo(obj, a) - distanceTo(obj, b)});
     return filtered[0];
+}
+
+const hasWall = (objX, objY) => {
+    const walls = objects.filter(obj => obj.type === 'wall');
+    for (const wall of walls) {
+        const {x, y, size} = wall;
+        if (x < objX && objX < x + size && y < objY && objY < y + size) {
+            return true;
+        }
+    }
+    return false;
 }
 
 function makeGrid(xSize, ySize, zHeight, gridColor) {
@@ -62,8 +91,18 @@ function clearObjects() {
     objects = [];
 }
 
-function addRandomObject(team) {
+function addWall(x, y) {
     objects.push({
+        type: 'wall',
+        x,
+        y,
+        size: 1,
+    })
+}
+
+function addRandomPlayer(team) {
+    objects.push({
+        type: 'player',
         id: _id++,
         team,
         x: Math.random() * 10,
@@ -84,12 +123,23 @@ function draw() {
   requestAnimationFrame(draw);
   makeGrid(10, 10, 0, WHITE);
 
+  const orderedObjects = objects.sort((a,b) => zDistance(b) - zDistance(a));
+
   for (const obj of objects) {
-    const {x, y, size, angle, team} = obj;
-    const color = (team === 'rock' ? RED : (team === 'paper' ? GREEN : BLUE)); 
-    const position = Isomer.Point(x,y,0);
-    const block = Isomer.Shape.Prism(position, size, size, size);
-    iso.add(block.rotateZ(Isomer.Point(x+size/2, y+size/2, 0), angle), color);
+    const {type, x, y, size, angle, team} = obj;
+
+    if (type === 'player') {
+        const color = (team === 'rock' ? RED : (team === 'paper' ? GREEN : BLUE)); 
+        const position = Isomer.Point(x,y,0);
+        const block = Isomer.Shape.Prism(position, size, size, size);
+        iso.add(block.rotateZ(Isomer.Point(x+size/2, y+size/2, 0), angle), color);
+    }
+    else if (type === 'wall') {
+        const color = WHITE;
+        const position = Isomer.Point(x,y,0);
+        const block = Isomer.Shape.Prism(position, size, size, 0.2);
+        iso.add(block, color);
+    }
   }
 }
 
@@ -97,9 +147,10 @@ function logic() {
     const DISTANCE = 0.03;
     const TURN = 0.03;
     let teamChanges = {};
+    let players = objects.filter(obj => obj.type === 'player');
 
-    for (let idx=0; idx<objects.length; idx+=1) {
-        const obj = objects[idx];
+    for (let idx=0; idx<players.length; idx+=1) {
+        const obj = players[idx];
         if (!obj.commands) {
             continue;
         }
@@ -108,8 +159,10 @@ function logic() {
             if (cmd === 'forward') {
                 const dx = Math.cos(obj.angle) * DISTANCE;
                 const dy = Math.sin(obj.angle) * DISTANCE;
-                obj.x += dx;
-                obj.y += dy;
+                if (!hasWall(obj.x + dx, obj.y + dy) && !hasWall(obj.x + dx + obj.size, obj.y + dy) && !hasWall(obj.x + dx, obj.y + dy + obj.size) && !hasWall(obj.x + dx + obj.size, obj.y + dy + obj.size)) {
+                    obj.x += dx;
+                    obj.y += dy;
+                }
             }
             else if (cmd === 'left') {
                 obj.angle = (obj.angle + TURN);
@@ -122,13 +175,11 @@ function logic() {
             }
         }
 
-        for (let jdx=idx+1; jdx<objects.length; jdx+=1) {
-            const other = objects[jdx];
-            if (obj.team !== other.team) {
-                const distance = distanceTo(obj.x, obj.y, other.x, other.y);
-                if (distance < obj.size + other.size) {
-                    teamChanges[other.id] = obj.team;
-                }
+        for (let jdx=idx+1; jdx<players.length; jdx+=1) {
+            const other = players[jdx];
+            const distance = distanceTo(obj, other);
+            if (obj.team !== other.team && isEnemy(obj, other) && distance < obj.size + other.size) {
+                teamChanges[other.id] = obj.team;
             }
         }
         
@@ -144,19 +195,15 @@ function logic() {
     }
 }
 
-function evalInContext(js, context) {
-    let x = (() => { return eval(js); }).call(context);
-}
-
 function ai() {
-    for (const obj of objects) {
+    for (let obj of objects.filter(o => o.type === 'player')) {
         obj.commands = [];
         let body = '';
         if (obj.team === 'rock') {
             body = airock.value;
         } else if (obj.team === 'scissors') {
             body = aiscissors.value;
-        } else {
+        } else if (obj.team === 'paper') {
             body = aipaper.value;
         }
         try {
@@ -165,6 +212,7 @@ function ai() {
             obj.commands = result;
         }
         catch {
+            console.log('failed ai', obj)
             // pass
         }
     }
@@ -173,10 +221,17 @@ function ai() {
 // ---
 function restart() {
     clearObjects();
-    for (let i=0; i<10; i++) {
-        addRandomObject('rock');
-        addRandomObject('scissors');
-        addRandomObject('paper');
+    addWall(3, 3);
+    addWall(5, 5);
+    addWall(7, 7);
+    for (let i=0; i<countrock.value; i++) {
+        addRandomPlayer('rock');
+    }
+    for (let i=0; i<countscissors.value; i++) {
+        addRandomPlayer('scissors');
+    }
+    for (let i=0; i<countpaper.value; i++) {
+        addRandomPlayer('paper');
     }
 }
 
